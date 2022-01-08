9893278bfb6f9a0df33322c25528c2f2c7edf9af
enar <nishchhalraturi@gmail.com>
2022-01-08 22:09:27 +0530

    Added tables in markets page

--- tracker.py
+++ tracker.py
@@ -8,4 +8,13 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-    return render_template('markets.html')
+    
+    items = [
+    {'id': 1, 'Country': 'India', 'Name': 'Nifty 50', 'price': 'niftyval'},
+    {'id': 2, 'Country': 'India', 'Name': 'Sensex', 'price': 'sensexval'},
+    {'id': 3, 'Country': 'USA', 'Name': 'S&P 500', 'price': 'snpval'},
+    {'id': 4, 'Country': 'USA', 'Name': 'Dow Jones', 'price': 'dawval'},
+    {'id': 5, 'Country': 'Worldwide', 'Name': 'BTC', 'price': 'btcval'},
+    {'id': 6, 'Country': 'Worldwide', 'Name': 'ETH', 'price': 'ethval'}
+    ]
+    return render_template('markets.html', items=items)
